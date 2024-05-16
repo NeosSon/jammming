596afe7eb1d02447d2598df2765db97a16c77f61
Neosson <gustavopena0825@gmail.com>
2024-05-16 07:10:37 -0400

    Disable multiple login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import { Tracks } from "./components/Tracks";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   // constants
   const client_id = "a1c008a11f5b4dd4b37d3a9fd04fee17";
   const client_secret = "f8c1dc89ed1f444aa142deab3694fc4f";
   const baseUrl = "https://api.spotify.com";
   const redirect_uri = "http://localhost:5173/callback";
-
+  const currentUrl = window.location.href;
+  let loggedIn = false;
+  if (currentUrl.includes("access_token")) {
+    loggedIn = true;
+  }
   // To get access token
   function login() {
+    if (loggedIn) {
+      return;
+    }
     const responseType = "token&show_dialog=true";
-
+    
     const endpoint = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-email",
@@ -34,8 +40,10 @@ function App() {
     window.location.href = `${endpoint}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
       " "
     )}&response_type=${responseType}`;
-  }
 
+    
+   }
+   
   //After function call
   const hash = window.location.hash;
   const token = hash.substring(1).split("&")[0].split("=")[1];
